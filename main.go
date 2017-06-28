9ac4ba8c317735ec731282da0d5dfbee2e56aa33
Pratheek Adidela <apratheek@unaxiom.com>
2017-06-28 21:52:59 +0530

    Code has been refactored to use appropriate internal mutex locks.

--- main.go
+++ main.go
@@ -23,26 +23,26 @@ import (
 // log is the ulogger object
 var log *ulogger.Logger
 
-var currentAvailableMemory = float64(12)
+// currentAvailableMemory stores the available memory
+var currentAvailableMemory memory
 
 // memoryRequiredPerProcess stores the memory required for running each process, in megabytes
 var memoryRequiredPerProcess float64
 
 // pipeline consists of all the list of semaphores that are currently available
-var pipeline []semaphore
+var pipeline semaphoreList
 
 // online consists of all the semaphores that are under execution
-var online []semaphore
-
-// pipelineMutex is the mutex that needs to be acquired before performing any operations on the pipeline array
-var pipelineMutex = &sync.Mutex{}
+var online semaphoreList
 
 // Setup accepts the average memory requirement for a process
 func Setup(memoryRequiredInMB float64) {
 	memoryRequiredPerProcess = memoryRequiredInMB
 	log = ulogger.New()
-	log.SetLogLevel(ulogger.DebugLevel)
+	// log.SetLogLevel(ulogger.DebugLevel)
+	log.SetLogLevel(ulogger.FatalLevel)
 	log.Debugln("Set memoryRequiredPerProcess to ", memoryRequiredPerProcess)
+	currentAvailableMemory.set(float64(12))
 }
 
 // Req accepts a string channel via which an access token is returned. The caller function can then begin its execution.
@@ -53,67 +53,38 @@ func Req(returnChan chan string) {
 	log.Debugln("Generated new token --> ", ticket.Token)
 	ticket.CallerChan = returnChan
 	ticket.Type = "" // This can be implemented in a later version
-	ticket.Seat = make(chan int, 1)
-	// Insert a dummy value into the seat
-	ticket.Seat <- 0
-	go func() {
-		// Insert the actual operating value into the seat
-		ticket.Seat <- 1
-	}()
-	// Attach this ticket to the global list of tickets
-	pipelineMutex.Lock()
-	defer pipelineMutex.Unlock()
-	pipeline = append(pipeline, ticket)
+	pipeline.append(ticket)
 	go processNextTicket()
 }
 
 // processNextTicket processes the next available ticket in the pipeline
 func processNextTicket() {
 	log.Errorln("Started processNextTicket()")
-	pipelineMutex.Lock()
-	defer pipelineMutex.Unlock()
 	var ticket semaphore
-	if len(pipeline) == 0 {
+	if pipeline.length() == 0 {
 		log.Errorln("Length of pipeline is 0!")
 		return
 	}
-	ticket = pipeline[0]
+	ticket = pipeline.getOne()
 	// Check here if memory is available
-	if memoryRequiredPerProcess > checkAvailableMemory() {
+	if memoryRequiredPerProcess > currentAvailableMemory.get() {
 		log.Errorln("Ticket --> ", ticket.Token, " does not have sufficient memory to process...")
 		return
 	}
 	// Process can be run here
+	currentAvailableMemory.reduce(memoryRequiredPerProcess)
 	ticket.CallerChan <- ticket.Token
-	pipeline = pipeline[1:]
-	online = append(online, ticket)
+	pipeline.remove(ticket)
+	online.append(ticket)
+	log.Warningln("Pipeline is ", pipeline.list, "\nAnd Online is ", online.list)
 }
 
 // Rel accepts the allotted token to the process and removes it from memory; consequently, it processes the next available process
 func Rel(token string) {
-	pipelineMutex.Lock()
 	log.Debugln("Trying to release token --> ", token)
-	defer pipelineMutex.Unlock()
-	for i, ticket := range online {
-		if ticket.Token == token {
-
-			online = append(online[0:i], online[i+1:]...)
-			currentAvailableMemory += memoryRequiredPerProcess
-			log.Debugln("Released token --> ", ticket.Token, " and current memory is ", currentAvailableMemory)
-			go processNextTicket()
-			return
-		}
-	}
-}
-
-// checkAvailableMemory returns the available memory as a float64
-func checkAvailableMemory() float64 {
-	currentAvailableMemory -= memoryRequiredPerProcess
-	if currentAvailableMemory <= 0 {
-		currentAvailableMemory = float64(0)
-	}
-	log.Debugln("Current available memory is ", currentAvailableMemory)
-	return currentAvailableMemory
+	online.removeByToken(token)
+	currentAvailableMemory.add(memoryRequiredPerProcess)
+	go processNextTicket()
 }
 
 type semaphore struct {
@@ -121,4 +92,90 @@ type semaphore struct {
 	CallerChan chan string // Stores the channel that will be used to return the Token
 	Seat       chan int    // This is the actual semaphore
 	Type       string      // This can be used later on, to store the type of this semaphore
+}
+
+// =============================================================================
+// 		memory struct
+type memory struct {
+	sync.RWMutex
+	available float64
+}
+
+// set sets the available memory to value passed
+func (m *memory) set(amount float64) {
+	m.Lock()
+	defer m.Unlock()
+	m.available = amount
+}
+
+// get gets the available memory
+func (m *memory) get() float64 {
+	m.Lock()
+	defer m.Unlock()
+	return m.available
+}
+
+// add adds the passed value to available memory
+func (m *memory) add(amount float64) {
+	m.Lock()
+	defer m.Unlock()
+	m.available += amount
+}
+
+// reduce removes the passed value from the available memory
+func (m *memory) reduce(amount float64) {
+	m.Lock()
+	defer m.Unlock()
+	m.available -= amount
+}
+
+// =============================================================================
+// 		semaphoreList struct
+type semaphoreList struct {
+	sync.Mutex
+	list []semaphore
+}
+
+func (l *semaphoreList) append(s semaphore) {
+	l.Lock()
+	defer l.Unlock()
+	l.list = append(l.list, s)
+}
+
+func (l *semaphoreList) remove(s semaphore) {
+	l.Lock()
+	defer l.Unlock()
+	for i, sem := range l.list {
+		if sem == s {
+			l.list = append(l.list[:i], l.list[i+1:]...)
+			break
+		}
+	}
+}
+
+// removeByToken removes the semaphore from the list where the token matches
+func (l *semaphoreList) removeByToken(token string) {
+	l.Lock()
+	defer l.Unlock()
+	for i, sem := range l.list {
+		if sem.Token == token {
+			l.list = append(l.list[:i], l.list[i+1:]...)
+			break
+		}
+	}
+}
+
+// Returns the length of the semaphore list
+func (l *semaphoreList) length() int {
+	l.Lock()
+	defer l.Unlock()
+	length := len(l.list)
+	return length
+}
+
+// getOne returns the first semaphore of the list
+func (l *semaphoreList) getOne() semaphore {
+	l.Lock()
+	defer l.Unlock()
+	return l.list[0]
 }
