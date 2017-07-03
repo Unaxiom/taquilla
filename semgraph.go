13cfe319fdd638ec60693be9db238c764d91e14c
Pratheek Adidela <apratheek@unaxiom.com>
2017-07-03 19:42:16 +0530

    Renamed structs.

--- semgraph.go
+++ semgraph.go
@@ -3,28 +3,28 @@ package taquilla
 import "sync"
 
 // ===========================================================================
-// semGraph describes a struct that holds the memory characteristics of each process
-type semGraph struct {
+// semGraphStruct describes a struct that holds the memory characteristics of each process
+type semGraphStruct struct {
 	sync.RWMutex
-	container map[string]*semCounter
+	container map[string]*semCounterStruct
 }
 
 // set sets the average memory of the specified semaphore
-func (s *semGraph) set(semName string) {
+func (s *semGraphStruct) set(semName string) {
 	s.Lock()
 	defer s.Unlock()
 	s.container[semName].updateAvg()
 }
 
-// semCounter stores the average memory used by each process, as well as a counter to denote the number of such processes that have already run
-type semCounter struct {
+// semCounterStruct stores the average memory used by each process, as well as a counter to denote the number of such processes that have already run
+type semCounterStruct struct {
 	sync.RWMutex
 	avgMem  uint64
 	counter uint64
 }
 
 // updateAvg increases the counter by 1, and calculates the average memory for this counter variable
-func (c *semCounter) updateAvg() {
+func (c *semCounterStruct) updateAvg() {
 	c.Lock()
 	defer c.Unlock()
 }
