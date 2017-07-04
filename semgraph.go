defe3d7f6398a4d982ed2cc74d63896c0a0de734
Pratheek Adidela <apratheek@unaxiom.com>
2017-07-04 21:22:30 +0530

    In semGraphStruct, a new string list has been introduced that holds all the different types of tickets. This is to ensure that an order is maintained when looping over the struct while calculating the equation matrix values.

--- semgraph.go
+++ semgraph.go
@@ -6,7 +6,8 @@ import "sync"
 // semGraphStruct describes a struct that holds the memory characteristics of each process
 type semGraphStruct struct {
 	sync.RWMutex
-	container map[string]*semCounterStruct
+	container      map[string]*semCounterStruct
+	availableTypes []string // Stores all the available types in a sequence
 }
 
 // set sets the average memory of the specified semaphore
@@ -17,6 +18,7 @@ func (s *semGraphStruct) set(ticketType string) {
 	if s.container[ticketType] == nil {
 		// Otherwise, create a new entry here
 		// log.Errorln("Creating new entry in semGraphStruct")
+		s.availableTypes = append(s.availableTypes, ticketType)
 		var newCounter = new(semCounterStruct)
 		newCounter.name = ticketType
 		s.container[ticketType] = newCounter
@@ -25,12 +27,12 @@ func (s *semGraphStruct) set(ticketType string) {
 	s.container[ticketType].incrementCounter()
 }
 
-// // incrementTicketCount increments the counter value (atomically) of the specific ticketType by 1
-// func (s *semGraphStruct) incrementTicketCount(ticketType string) {
-// 	s.Lock()
-// 	defer s.Unlock()
-// 	s.container[ticketType].incrementCounter()
-// }
+// len returns the number of keys in the container
+func (s *semGraphStruct) len() int {
+	s.RLock()
+	defer s.RUnlock()
+	return len(s.availableTypes)
+}
 
 // semCounterStruct stores the average memory used by each process, as well as a counter to denote the number of such processes that have already run
 type semCounterStruct struct {
@@ -40,16 +42,10 @@ type semCounterStruct struct {
 	name    string
 }
 
-// updateAvg increases the counter by 1, and calculates the average memory for this counter variable
-// func (c *semCounterStruct) updateAvg() {
-// 	c.Lock()
-// 	defer c.Unlock()
-// }
-
 // incrementCounter atomically increases the counter value by 1
 func (c *semCounterStruct) incrementCounter() {
 	// c.Lock()
 	// defer c.Unlock()
 	c.counter++
-	// log.Errorln("Counter for ", c.name, " has become ", c.counter)
+	log.Errorln("Counter for ", c.name, " has become ", c.counter)
 }
