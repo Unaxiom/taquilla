8cd238929b6f50b305f878f2d11ceff9e046cfca
Pratheek Adidela <apratheek@unaxiom.com>
2017-07-04 21:20:19 +0530

    3 different ticket types are now being generated in the example.

--- example/example.go
+++ example/example.go
@@ -14,29 +14,40 @@ var log *ulogger.Logger
 
 func init() {
 	log = ulogger.New()
-	taquilla.Setup(float64(7))
+	taquilla.Setup(float64(2))
 }
 
 func main() {
 	// ch := make(chan int)
 	var wg sync.WaitGroup
 	ticketType := uuid.NewV4().String()
-	for i := 0; i < 3; i++ {
+	for i := 0; i < 10; i++ {
 		wg.Add(1)
 		go func(count int) {
 			token := taquilla.Req(ticketType)
-			<-time.After(time.Second * time.Duration(3))
+			<-time.After(time.Second * time.Duration(4))
 			log.Infoln("Token received in example is ", token, " for count --> ", count)
 			taquilla.Rel(token)
 			wg.Done()
 		}(i)
 	}
 	ticketType2 := uuid.NewV4().String()
-	for i := 3; i < 5; i++ {
+	for i := 10; i < 20; i++ {
 		wg.Add(1)
 		go func(count int) {
 			token := taquilla.Req(ticketType2)
-			<-time.After(time.Second * time.Duration(3))
+			<-time.After(time.Second * time.Duration(4))
+			log.Infoln("Token received in example is ", token, " for count --> ", count)
+			taquilla.Rel(token)
+			wg.Done()
+		}(i)
+	}
+	ticketType3 := uuid.NewV4().String()
+	for i := 10; i < 20; i++ {
+		wg.Add(1)
+		go func(count int) {
+			token := taquilla.Req(ticketType3)
+			<-time.After(time.Second * time.Duration(4))
 			log.Infoln("Token received in example is ", token, " for count --> ", count)
 			taquilla.Rel(token)
 			wg.Done()
@@ -44,4 +55,5 @@ func main() {
 	}
 	// <-ch
 	wg.Wait()
+	<-time.After(time.Second * time.Duration(10))
 }
