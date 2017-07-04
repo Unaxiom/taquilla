543ac9eec7bd4670cce8c7aec7010ee9b8161762
Pratheek Adidela <apratheek@unaxiom.com>
2017-07-04 21:26:29 +0530

    get() has been renamed to getFree() to better reflect the nature of the method. Also, a new variable - total has been added that stores the total memory available in the system. This can later be removed, if redundant.

--- memory.go
+++ memory.go
@@ -6,6 +6,7 @@ import "sync"
 // 		memory struct
 type memory struct {
 	sync.RWMutex
+	total     float64
 	available float64
 }
 
@@ -13,11 +14,12 @@ type memory struct {
 func (m *memory) set(amount float64) {
 	m.Lock()
 	defer m.Unlock()
+	m.total = amount
 	m.available = amount
 }
 
-// get gets the available memory
-func (m *memory) get() float64 {
+// getFree gets the available memory
+func (m *memory) getFree() float64 {
 	m.Lock()
 	defer m.Unlock()
 	log.Debugln("Current memory is ", m.available)
