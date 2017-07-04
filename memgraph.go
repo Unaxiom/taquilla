98978a2aa01811f997b801bb390da187b6506c6b
Pratheek Adidela <apratheek@unaxiom.com>
2017-07-04 21:25:25 +0530

    In sysMemAndOnlineSem, instead of holding a list of the online semaphore structs, just their ticket types are being stored. This makes it easier to debug. Plus, the additional info is redundant. Also, a new method updateMem() has been written that generates the equation matrix and the result matriex from the available data.

--- memgraph.go
+++ memgraph.go
@@ -1,6 +1,11 @@
 package taquilla
 
-import "sync"
+import (
+	"fmt"
+	"sync"
+
+	"github.com/gonum/matrix/mat64"
+)
 
 // ===========================================================================
 // memGraph describes a struct that holds the sys allocated memory
@@ -10,12 +15,12 @@ type memGraph struct {
 }
 
 // set acquires an exclusive lock and updates the map with the current timestamp and the sys memory used
-func (m *memGraph) set(timestamp int64, sysMem uint64) {
+func (m *memGraph) set(timestamp int64, sysMem uint64, onlineTypes []string) {
 	m.Lock()
 	defer m.Unlock()
 	var localvar sysMemAndOnlineSem
 	localvar.sysMem = sysMem
-	localvar.online = online.getAll()
+	localvar.online = onlineTypes
 	m.value[timestamp] = localvar
 	// log.Errorln("\n\n================================\nMem Graph is\n", len(localvar.online), "\n\n=================================")
 }
@@ -27,8 +32,56 @@ func (m *memGraph) get(timestamp int64) sysMemAndOnlineSem {
 	return m.value[timestamp]
 }
 
+// updateMem calculates the memory required for each semaphore by solving the N-dimensional equation formed from constant monitoring of the memory used by the app
+func (m *memGraph) updateMem() {
+	m.RLock()
+	defer m.RUnlock()
+	// Generate equation matrix here
+	// The different types of semaphores are given by the number of keys of the semGraphStruct --> this will also be the number of variables in an equation --> number of columns in the matrix
+	numberOfColumnsOfEqn := semGraph.len()
+	numberOfRowsOfEqn := len(m.value)
+	eqnMatrix := mat64.NewDense(numberOfRowsOfEqn, numberOfColumnsOfEqn, nil)
+	resultMatrix := mat64.NewDense(numberOfRowsOfEqn, 1, nil)
+	// eqnMatrix.Set()
+	semGraph.RLock()
+	defer semGraph.RUnlock()
+
+	// Outer loop is memGraph.value keys
+	var rowCount = 0
+	for timestamp, sysMem := range m.value {
+		fmt.Println("*******")
+		// fmt.Println("Timestamp: ", timestamp, " Row: ", rowCount, " ")
+		resultMatrix.SetRow(rowCount, []float64{float64(sysMem.sysMem)})
+		var equationRow []float64
+		for _, ticketType := range semGraph.availableTypes {
+			// fmt.Println("Ticket Type --> ", ticketType)
+			var semFoundCounter = 0
+			for _, semType := range sysMem.online {
+				if ticketType == semType {
+					// fmt.Println("Found ticketType...")
+					semFoundCounter++
+				}
+			}
+			equationRow = append(equationRow, float64(semFoundCounter))
+
+		}
+		eqnMatrix.SetRow(rowCount, equationRow)
+		fmt.Println("Timestamp: ", timestamp, " Row: ", rowCount, ", ", equationRow)
+		// fmt.Println("Equation Row is ", equationRow)
+		fmt.Println("*******")
+		rowCount++
+	}
+
+	log.ErrorDump("=======================================================================================")
+	log.ErrorDump("Eqn Matrix is: ", eqnMatrix, "\n\n")
+	log.ErrorDump("Result Matrix is: ", resultMatrix, "\n\n")
+	log.ErrorDump("=======================================================================================")
+	go computeEquationSolution(eqnMatrix, resultMatrix)
+
+}
+
 // sysMemAndOnlineSem is a struct that stores the system memory as well as the list of online semaphores running at that particular timestamp
 type sysMemAndOnlineSem struct {
 	sysMem uint64
-	online []semaphore
+	online []string
 }
