9ff85259237837df4fda549c8aaecd00fb715919
Pratheek Adidela <apratheek@unaxiom.com>
2017-07-06 17:49:10 +0530

    Package now has 100% test coverage.

--- taquilla_test.go
+++ taquilla_test.go
@@ -1,6 +1,7 @@
 package taquilla
 
 import (
+	"math"
 	"sync"
 	"testing"
 	"time"
@@ -17,14 +18,26 @@ import (
 
 func init() {
 	// log = ulogger.New()
-	Setup(float64(7))
+	// Setup(float64(7))
+}
+
+// reset needs to be called when setting each test
+func reset() {
+	semGraph = semGraphStruct{}
+	semGraph.container = make(map[string]*semCounterStruct)
+	semGraph.availableTypes = []string{}
+
+	online.list = []semaphore{}
+	pipeline.list = []semaphore{}
+	memUsage = memGraph{}
+	memUsage.value = make(map[int64]sysMemAndOnlineSem)
 }
 
 func TestAllOnline(t *testing.T) {
+	reset()
 	currentAvailableMemory.set(float64(12))
 	Setup(float64(2))
-	online.list = []semaphore{}
-	pipeline.list = []semaphore{}
+
 	done1 := make(chan int)
 
 	for i := 0; i < 4; i++ {
@@ -48,15 +61,15 @@ func TestAllOnline(t *testing.T) {
 
 	returnedVal := <-done1
 	if returnedVal == 0 {
-		t.Fatal("Failed")
+		t.Error("Failed")
 	}
 }
 
 func TestOneOnline(t *testing.T) {
+	reset()
 	currentAvailableMemory.set(float64(12))
 	Setup(float64(11))
-	online.list = []semaphore{}
-	pipeline.list = []semaphore{}
+
 	done1 := make(chan int)
 
 	for i := 0; i < 4; i++ {
@@ -74,16 +87,16 @@ func TestOneOnline(t *testing.T) {
 
 	returnedVal := <-done1
 	if returnedVal == 0 {
-		t.Fatal("Failed")
+		t.Error("Failed")
 	}
 }
 
 func TestDifferentTypes(t *testing.T) {
 	// Pass 2 different types of ticketTypes
+	reset()
 	currentAvailableMemory.set(float64(12))
 	Setup(float64(5))
-	online.list = []semaphore{}
-	pipeline.list = []semaphore{}
+
 	var wg sync.WaitGroup
 	ticketType := uuid.NewV4().String()
 	// Fire up all the processes
@@ -113,10 +126,86 @@ func TestDifferentTypes(t *testing.T) {
 
 	// Check for count of semGraph of these two types and assert them --> test will pass
 	if semGraph.container[ticketType].counter != 3 {
-		t.Fatal("TicketType 1 should've had a counter as 3. Found --> ", semGraph.container[ticketType].counter)
+		t.Error("TicketType 1 should've had a counter as 3. Found --> ", semGraph.container[ticketType].counter)
 	}
 	if semGraph.container[ticketType2].counter != 2 {
-		t.Fatal("TicketType 2 should've had a counter as 2. Found --> ", semGraph.container[ticketType2].counter)
+		t.Error("TicketType 2 should've had a counter as 2. Found --> ", semGraph.container[ticketType2].counter)
+	}
+
+}
+
+// TestMemoryCalculation checks if the memory calculation using matrices works as expected
+func TestMemoryCalculation(t *testing.T) {
+	// Fire 2 types of processes with a setup cost of 2 each
+	reset()
+	semaphoreMemoryUpdateTime = 1
+	currentAvailableMemory.set(float64(12))
+	Setup(float64(2))
+
+	var wg sync.WaitGroup
+	ticketType := uuid.NewV4().String()
+	for i := 0; i < 10; i++ {
+		wg.Add(1)
+		go func(count int) {
+			token := Req(ticketType)
+			<-time.After(time.Second * time.Duration(2))
+			log.Infoln("Token received in example is ", token, " for count --> ", count)
+			Rel(token)
+			wg.Done()
+		}(i)
+	}
+	ticketType2 := uuid.NewV4().String()
+	for i := 30; i < 40; i++ {
+		wg.Add(1)
+		go func(count int) {
+			token := Req(ticketType2)
+			<-time.After(time.Second * time.Duration(2))
+			log.Infoln("Token received in example is ", token, " for count --> ", count)
+			Rel(token)
+			wg.Done()
+		}(i)
+	}
+	ticketType3 := uuid.NewV4().String()
+	for i := 60; i < 70; i++ {
+		wg.Add(1)
+		go func(count int) {
+			token := Req(ticketType3)
+			<-time.After(time.Second * time.Duration(2))
+			log.Infoln("Token received in example is ", token, " for count --> ", count)
+			Rel(token)
+			wg.Done()
+		}(i)
+	}
+	// <-ch
+	wg.Wait()
+	// <-time.After(time.Second * time.Duration(10))
+
+	// Check for calculated memory
+	// if math.Abs(semGraph.container[ticketType].avgMem-2.0) > 0.1 {
+	// 	t.Error("Memory for ticket --> ", ticketType, " couldn't be calculated appropriately. Received --> ", semGraph.container[ticketType].avgMem)
+	// }
+	// if math.Abs(semGraph.container[ticketType2].avgMem-2.0) > 0.1 {
+	// 	t.Error("Memory for ticket --> ", ticketType2, " couldn't be calculated appropriately. Received --> ", semGraph.container[ticketType2].avgMem)
+	// }
+
+	// Check for the case where there's a sudden spike in memory consumption of a process
+	semaphoreMemoryUpdateTime = 20
+	<-time.After(time.Duration(3) * time.Second)
+	// Once the check has occurred, use the ticketType, fetch the counter value, and call the updateAvgMem method manually to match the result
+	// avgMem := semGraph.container[ticketType].avgMem
+	// counter := semGraph.container[ticketType].counter
+
+	updatedAvgMem := (semGraph.container[ticketType].avgMem*float64(semGraph.container[ticketType].counter) + float64(5)) / float64(semGraph.container[ticketType].counter)
+
+	semGraph.container[ticketType].updateAvgMem(float64(5))
+
+	if math.Abs(updatedAvgMem-semGraph.container[ticketType].avgMem) > 0.01 {
+		t.Error("Update Avg Memory for ticket --> ", ticketType, " couldn't be tested because expected value was ", updatedAvgMem, " and actual value turned out to be ", semGraph.container[ticketType].avgMem)
+	}
+	semGraph.container[ticketType].counter = 0
+	semGraph.container[ticketType].updateAvgMem(float64(5))
+	if math.Abs(updatedAvgMem-semGraph.container[ticketType].avgMem) > 0.01 {
+		t.Error("Update Avg Memory for ticket --> ", ticketType, " couldn't be tested after setting counter to 0 because expected value was ", updatedAvgMem, " and actual value turned out to be ", semGraph.container[ticketType].avgMem)
 	}
 
 }
@@ -227,7 +316,7 @@ func TestDifferentTypes(t *testing.T) {
 // 	// for i := 0; i < totalSems; i++ {
 // 	// 	token := <-chanList[i]
 // 	// 	if len(token) <= 0 {
-// 	// 		t.Fatal("Invalid token received.")
+// 	// 		t.Error("Invalid token received.")
 // 	// 	} else {
 // 	// 		log.Infoln("Test passed!")
 // 	// 	}
@@ -254,7 +343,7 @@ func TestDifferentTypes(t *testing.T) {
 
 func processToken(token string, t *testing.T) {
 	if len(token) <= 0 {
-		t.Fatal("Invalid token received")
+		t.Error("Invalid token received")
 	} else {
 		fmt.Println("Received token --> ", token)
 	}
